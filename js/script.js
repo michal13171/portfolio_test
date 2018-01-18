013409e581ce89569ae48e90bbf97d2f6cd71b0b
Michał Kukuła <michal13171@wp.pl>
2018-01-18 14:43:17 +0100

    pomin ify z Nan

--- js/script.js
+++ js/script.js
@@ -1,24 +1,59 @@
 'use strict';
+var divisionByZod = 42 / "General Zod";
+// This can only be true if the value is NaN
+var valueIsNaN = (divisionByZod !== divisionByZod);
+// Outputs: true
+console.log(valueIsNaN);
 $(document).ready(function () {
     //audio
     (function () {
         function audio() {
-            this._audioplayer = $('#player');
-            this._volume = $('.volume');
-            //  this._play = document.querySelector('.play_pause');;
+            this._audioplayer = document.getElementById('player');
+            this._volume = document.querySelector('.volume');
+            this._duration = document.querySelector('.current-time');
+            this._currentTime = document.querySelector('.full-time');
+            this._play = document.querySelector('.play_pause');
             this._progressBar = $('.progressbar').slider();
             this._Assignplay();
+            this._AssignVol();
+            this._time();
         }
         audio.prototype._Assignplay = function () {
             $('.play_pause').click('click', this._play_orStop.bind(this), false);
         };
+        audio.prototype._AssignVol = function () {
+            this._volume.addEventListener('click', this._muteUnmute.bind(this), false);
+        };
         audio.prototype._play_orStop = function () {
             if (this._audioplayer.paused || this._audioplayer.ended) {
-                this._audioplayer.get(0).play();
+                this._audioplayer.play();
+                this._play.innerHTML = '<img src="img/ic_play_circle_outline_black_48dp.png"/>';
             }
             else {
                 this._audioplayer.pause();
+                this._play.innerHTML = '<img src="img/ic_pause_circle_outline_black_48dp.png"/>';
             }
+        };
+        audio.prototype._muteUnmute = function (e) {
+            e.preventDefault();
+            if (this._audioplayer.muted === true) {
+                this._audioplayer.muted = false;
+                this._volume.innerHTML = '<img src="img/ic_volume_down_black_48dp.png"/>';
+            }
+            else {
+                this._audioplayer.muted = true;
+                this._volume.innerHTML = '<img src="img/ic_volume_mute_black_48dp.png"/>';
+            }
+        };
+        audio.prototype._time = function () {
+            this._minutes = Number.isNaN(parseInt(this._audioplayer.duration / 60));
+            this._seconds = Number.isNaN(parseInt(this._audioplayer.duration % 60));
+            if (this._minutes < 1 || this._minutes > 60) {
+                this._minutes = this._minutes.NaN;
+                console.log("Miesiąc musi być liczbą między 0 i 60." + this._minutes);
+            }
+            this._duration.innerHTML = this._minute + ' : ' + this._seconds;
+            console.log(this._minutes);
         };
         new audio();
     })();
