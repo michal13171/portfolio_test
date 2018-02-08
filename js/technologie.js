78d9bcc09d7ed12b06c9d5b80519c0217944e781
Michał Kukuła <michal13171@wp.pl>
2018-02-08 16:17:11 +0100

    initial

--- js/technologie.js
+++ js/technologie.js
@@ -1,4 +1,5 @@
 'use strict';
+<<<<<<< HEAD
 
 $(function () {
     $('.slider_technology').slick({
@@ -33,3 +34,27 @@ $(function () {
         ]
     });
 });
+=======
+var prev = document.querySelector('.previous_slider');
+var next = document.querySelector('.next_slider');
+next.addEventListener('click', skills, true);
+
+function skills() {
+    var lists = ['img/1 (1).jpeg', 'img/1 (2).jpeg', 'img/1 (3).jpeg'];
+    var Image1 = document.createElement('div');
+    Image1.src = lists;
+    var elem = document.querySelector('.slider_technology').appendChild(Image1);
+    var apppedm = [];
+    for (var i = 0; i < lists.length; i++) {
+        apppedm[i] = new Image();
+        apppedm[i].src = lists[i];
+        var one = Math.floor(lists.length) ;
+        elem.innerHTML =   apppedm[lists.src] ;
+        console.log(lists[i].src);
+        console.log(apppedm[i].src);
+        console.log(elem);
+        console.log(elem.innerHTML = '<img src="' + lists[i] + '"/>');
+    }
+
+}
+>>>>>>> 10c2bee431a15cf0c64c8489f8380d6f10e255f6
